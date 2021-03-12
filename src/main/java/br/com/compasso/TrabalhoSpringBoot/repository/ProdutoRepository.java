fcb9ef2ed6ecb9d7f7bfb846e36dd438745e8e7a
VictorB <barberinivitor@gmail.com>
2021-03-11 21:45:23 -0300

    adicionando findByDescricao

--- src/main/java/br/com/compasso/TrabalhoSpringBoot/repository/ProdutoRepository.java
+++ src/main/java/br/com/compasso/TrabalhoSpringBoot/repository/ProdutoRepository.java
@@ -1,8 +1,12 @@
 package br.com.compasso.TrabalhoSpringBoot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import br.com.compasso.TrabalhoSpringBoot.modelo.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
+	List<Produto> findByDescricao(String descricao);
 }
